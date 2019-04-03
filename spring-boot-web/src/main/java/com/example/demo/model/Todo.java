2d3e1143e6d63b26cc62b0129551bcf2a727c0db
hyejun8701 <hyejun8701@gmail.com>
2019-04-03 13:52:11 +0900

    gitignore 변경

--- spring-boot-web/src/main/java/com/example/demo/model/Todo.java
+++ spring-boot-web/src/main/java/com/example/demo/model/Todo.java
@@ -1,32 +1,20 @@
-package com.example.demo.model;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-@ApiModel
-public class Todo {
-	@ApiModelProperty(value="아이디")
-	private long id;
-	private String title;
-	
-	public Todo(long id, String title) {
-		this.id = id;
-		this.title = title;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-}
+/*
+ * package com.example.demo.model;
+ * 
+ * import io.swagger.annotations.ApiModel; import
+ * io.swagger.annotations.ApiModelProperty;
+ * 
+ * @ApiModel public class Todo {
+ * 
+ * @ApiModelProperty(value="아이디") private long id; private String title;
+ * 
+ * public Todo(long id, String title) { this.id = id; this.title = title; }
+ * 
+ * public long getId() { return id; }
+ * 
+ * public void setId(long id) { this.id = id; }
+ * 
+ * public String getTitle() { return title; }
+ * 
+ * public void setTitle(String title) { this.title = title; } }
+ */
--- spring-boot-web/src/test/java/com/example/demo/RestAPITest.java
+++ spring-boot-web/src/test/java/com/example/demo/RestAPITest.java
@@ -1,47 +1,39 @@
-package com.example.demo;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.example.demo.model.User;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringBootWebApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RestAPITest {
-	@Autowired
-	private TestRestTemplate restTemplate;
-	
-	@Test @Ignore
-	public void testGetUserById() {
-		String url = "http://localhost:8080/user/0";
-		User user = restTemplate.getForObject(url, User.class);
-		System.out.println("등록일:" + user.getRegDate() + "," + user.getUserId() + "," + user.getUname());
-	}
-	
-	@Test
-	public void testGetUsers() {
-		String url = "http://localhost:8080/user";
-		ResponseEntity<Map<String, List<User>>> result = restTemplate
-				.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, List<User>>>() {
-		});
-		Map<String, List<User>> tempMap = result.getBody();
-		
-		ArrayList<User> resultArr = (ArrayList<User>) tempMap.get("result");
-		for(User user : resultArr) {
-			System.out.println(user.getUname());
-		}
-	}
-
-}
+/*
+ * package com.example.demo;
+ * 
+ * import java.util.ArrayList; import java.util.List; import java.util.Map;
+ * 
+ * import org.junit.Ignore; import org.junit.Test; import
+ * org.junit.runner.RunWith; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.boot.test.context.SpringBootTest; import
+ * org.springframework.boot.test.web.client.TestRestTemplate; import
+ * org.springframework.core.ParameterizedTypeReference; import
+ * org.springframework.http.HttpMethod; import
+ * org.springframework.http.ResponseEntity; import
+ * org.springframework.test.context.junit4.SpringRunner;
+ * 
+ * import com.example.demo.model.User;
+ * 
+ * @RunWith(SpringRunner.class)
+ * 
+ * @SpringBootTest(classes = SpringBootWebApplication.class, webEnvironment =
+ * SpringBootTest.WebEnvironment.RANDOM_PORT) public class RestAPITest {
+ * 
+ * @Autowired private TestRestTemplate restTemplate;
+ * 
+ * @Test @Ignore public void testGetUserById() { String url =
+ * "http://localhost:8080/user/0"; User user = restTemplate.getForObject(url,
+ * User.class); System.out.println("등록일:" + user.getRegDate() + "," +
+ * user.getUserId() + "," + user.getUname()); }
+ * 
+ * @Test public void testGetUsers() { String url = "http://localhost:8080/user";
+ * ResponseEntity<Map<String, List<User>>> result = restTemplate .exchange(url,
+ * HttpMethod.GET, null, new ParameterizedTypeReference<Map<String,
+ * List<User>>>() { }); Map<String, List<User>> tempMap = result.getBody();
+ * 
+ * ArrayList<User> resultArr = (ArrayList<User>) tempMap.get("result"); for(User
+ * user : resultArr) { System.out.println(user.getUname()); } }
+ * 
+ * }
+ */
